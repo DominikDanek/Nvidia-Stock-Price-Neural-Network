107cc386f8bf06dc305687fefc3fab807b358d79
danekdo <danek.dominik@yahoo.com>
2026-06-03 18:24:42 +0100

    didnt save the file on the last commit

--- NeuralNet.cs
+++ NeuralNet.cs
@@ -7,9 +7,6 @@ class NeuralNetwork{
     double[] output_weights;
     double output_bias;
     double learning_rate = 0.01;
-    static int Forward(){
-        
-    }
     public NeuralNetwork(int input_size, int hidden_size){
         //hidden weights
         //hidden biases
@@ -27,14 +24,66 @@ class NeuralNetwork{
             hidden_biases[h] = randomWeight();
             output_weights[h] = randomWeight();
         }
+        output_bias = randomWeight();
     }
     double randomWeight(){
         return (random.NextDouble() *2) -1;
     }
     public double Forward(double[] inputs){
+        double[] hidden_outputs = new double[hidden_weights.GetLength(0)];
 
+        //hidden layer
+        for (int h = 0; h < hidden_outputs.Length; h++){
+
+            double sum = 0;
+            for (int i = 0; i < inputs.Length; i++){
+                sum +=inputs[i] * hidden_weights[h, i];
+            }
+            sum += hidden_biases[h];
+            hidden_outputs[h] = Sigmoid(sum);
+        }
+        //output layer
+        double output_sum = 0;
+        for (int h = 0; h < hidden_outputs.Length; h++){
+            output_sum += hidden_outputs[h] * hidden_weights[h];
+        }
+        output_sum += output_bias;
+
+        return sigma(output_sum);
     }
     public double Train(double[] inputs, double target){
+        double[] hidden_outputs = new double[hidden_weights.GetLength(0)];
+
+
+        //forward pass
+        for (int h = 0; h < hidden_outputs.Length; h++)
+        {
+            double sum = 0;
+            for (int i = 0; i < inputs.Length; i++){
+                sum += inputs[i] *hidden_weights[h, i];
+            }
+            sum += hidden_biases[h];
+
+            hidden_outputs[h] = Sigmoid(sum);
+        }
+        double output_sum = 0;
+
+        for (int h= 0; h < hidden_outputs.Length; h++){
+            output_sum = hidden_outputs[h]*output_weights[h];
+        }
+
+        output_sum+= output_bias;
+
+        double output = sigma(output_sum);
+        //calculate error
+        double error = target - output;
+
+        //output gradient
+        double output_gradient =error * sigmaDerivative(output);
+
+        for (int h = 0; h < hidden_outputs.Length; h++){
+            output_weights[h] += learning_rate *output_gradient *hidden_outputs[h];
+        }
 
     }
     double sigma(double x){
