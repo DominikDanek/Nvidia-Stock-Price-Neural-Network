fbf79da7793c24c0672afc92d9a58387b5afaf5c
danekdo <danek.dominik@yahoo.com>
2026-06-03 18:21:34 +0100

    adding functionality to train and feed forward. + Initialise weights function

--- NeuralNet.cs
+++ NeuralNet.cs
@@ -10,12 +10,23 @@ class NeuralNetwork{
     static int Forward(){
         
     }
-    public neuralNetwork(int input_size, int hidden_size){
+    public NeuralNetwork(int input_size, int hidden_size){
         //hidden weights
         //hidden biases
         //output weights
+        hidden_weights = new double[hidden_size, input_size];
+        hideen_biases = new double[hidden_size];
+        output_weights = new double[hidden_size];
+        initialiseWeights();
     }
     void initialiseWeights(){
+        for (int h = 0; h < GetLenght(0); h++){
+            for (int i = 0; i < GetLength(1); i++){
+                hidden_weights[h,i] = randomWeight();
+            }
+            hidden_biases[h] = randomWeight();
+            output_weights[h] = randomWeight();
+        }
     }
     double randomWeight(){
         return (random.NextDouble() *2) -1;
