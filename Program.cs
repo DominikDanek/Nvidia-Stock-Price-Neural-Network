d83d333004b25768393669ef54f03fbc6940e35b
danekdo <danek.dominik@yahoo.com>
2026-06-04 14:05:11 +0100

    Filling in Load prices and build training data functionality

--- Program.cs
+++ Program.cs
@@ -17,15 +17,40 @@ class Program{
         List<double> prices = LoadPrices("data.csv"); // going to be the function to fetch the prices
         int window_size = 5;
 
-    List<(double[] inputs, double target)> training_data = BuildTrainingData(prices, window_size);
+        List<(double[] inputs, double target)> training_data = BuildTrainingData(prices, window_size);
 
-    NeuralNetwork network = new NeuralNetwork(input_size: 5, hidden_size: 4);
+        NeuralNetwork network = new NeuralNetwork(input_size: 5, hidden_size: 4);
 
-    for (int i = 0; i<1000; i++){
-        double total_error=0;
-        foreach (var sample in training_data){
-            total_error += network.Train(sample.inputs, sample.target);
+        for (int i = 0; i<1000; i++){
+            double total_error=0;
+            foreach (var sample in training_data){
+                total_error += network.Train(sample.inputs, sample.target);
+            }
+            if (i %100 == 0){
+                Console.WriteLine($"Epoch {i} error: {total_error:F4}");
+            }
+        }
+        Console.WriteLine("\n ---Predictions-- \n");
+
+        foreach (var sample in training_data.Take(10)){
+            double prediction = network.Forward(sample.inputs);
+            Console.WriteLine($"Prediction: {prediction:F4} | Target: {sample.target}");
         }
     }
+    static List<double> LoadPrices(string path){
+        List<double> prices = new();
+
+        string[] lines = File.ReadAllLines(path);
+
+        for (int i = 0; i < lines.Length; i++){
+            string[] columns = lines[i].Split(',');
+
+            double close = double.Parse(columns[1], CultureInfo.InvariantCulture);
+            prices.Add(close);
+        }
+        return prices;
+    }
+    static List<(double[] inputs, double target)> BuildTrainingData(List<double> prices, int window_size){
+        List<(double[] Inputs, double Target)> data = new();
     }
 }
